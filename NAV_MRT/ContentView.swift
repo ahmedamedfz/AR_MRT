3c9c0588aa6fa2fae91f4c8af8c2bc77b786817f
ahmadamedfz <ahmadfariz11@gmail.com>
2023-07-17 14:17:49 +0700

    Initial

---
+++ NAV_MRT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NAV_MRT
+//
+//  Created by Ahmad Fariz on 17/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NAV_MRT/NAV_MRTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAV_MRTApp.swift
+//  NAV_MRT
+//
+//  Created by Ahmad Fariz on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NAV_MRTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
